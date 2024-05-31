0824e4a3ae1c2c5f1a62fffa095f4c4ca054cce6
MarcoRomanoHub <marcoromano270@gmail.com>
2024-05-31 16:07:12 +0200

    [add] milestone 2

--- app/Http/Controllers/Api/PageController.php
+++ app/Http/Controllers/Api/PageController.php
@@ -4,11 +4,14 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Project;
 
 
 class PageController extends Controller
 {
     public function index()
     {
+        $projects = Project::all();
+        return response()->json($projects);
     }
 }
